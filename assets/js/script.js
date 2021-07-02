85230304fe6094bbd45a112423a2151b6d2bbe0f
Stephanie Hage <stephaniehage23@gmail.com>
2021-07-01 20:49:10 -0400

    api's

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,28 @@
-let apiKey = "N6jC9hEJzTF9RnctCg_sNYHsnJeGGqXljv7PadDwa9cnNkH1l-dyPYCqUZ3j6JFyEBP9kfiiGvNbjdGloQd-0trLXbfSMkA69e1gvRnJM3q5ps_T1Z7-yZxkWg7dYHYx"
+var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=pizza&location=cleveland";
+$.ajax({
+    url: myurl,
+    headers: {
+        'Authorization': 'Bearer N6jC9hEJzTF9RnctCg_sNYHsnJeGGqXljv7PadDwa9cnNkH1l-dyPYCqUZ3j6JFyEBP9kfiiGvNbjdGloQd-0trLXbfSMkA69e1gvRnJM3q5ps_T1Z7-yZxkWg7dYHYx',
+    },
 
-//format the yelp api url
-var apiUrl = "https://api.yelp.com/v3/businesses/search?location=Cleveland&radius=40000"
+    method: 'GET',
+    dataType: 'json',
+    success: function (data) {
+        console.log(data)
+    }
+});
 
-//make a request to the url
-
-fetch(apiUrl, {
-    headers: new Headers({
-        Authorization: "Bearer " + apiKey,
-        "Access-Control-Allow-Origin" : "*"
-    }),
-    method: "GET",
-    mode:"cors"
-})
-.then((response) => response.json())
-.then((data) => console.log(data));
+$.ajax({
+    type: "GET",
+    url: "https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=6XzGGxlIpYQAZnWYPnzYpZDK59vJeJId",
+    async: true,
+    dataType: "json",
+    success: function (json) {
+        console.log(json);
+        // Parse the response.
+        // Do other things.
+    },
+    error: function (xhr, status, err) {
+        // This time, we do not end up here!
+    }
+});

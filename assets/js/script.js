e6b0263c0b2d8d60f4b69b8d313a24a1c7ac88fc
Stephanie Hage <stephaniehage23@gmail.com>
2021-07-06 22:54:16 -0400

    fixed random function in fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,67 +1,52 @@
-function getYelpData(searchTerm, searchRadius, searchType){
+function getYelpData(searchTerm, searchRadius, searchType) {
     var myurl = ""
-    if(searchType === "food"){
-        myurl = "http://api.yelp.com/v3/businesses/search?term=food&limit=50&location=" + searchTerm+ "&radius=" + searchRadius;
+    if (searchType === "food") {
+        myurl = "http://cors-anywhere.herokuapp.com/http://api.yelp.com/v3/businesses/search?term=food&limit=50&location=" + searchTerm + "&radius=" + searchRadius;
     }
-    if(searchType === "bar"){
-        myurl = "http://api.yelp.com/v3/businesses/search?term=bar&limit=50&location=" + searchTerm+ "&radius=" + searchRadius;
+    if (searchType === "bar") {
+        myurl = "http://cors-anywhere.herokuapp.com/http://api.yelp.com/v3/businesses/search?term=bar&limit=50&location=" + searchTerm + "&radius=" + searchRadius;
     }
     //generate random restraurant (referring to this as "food" cause easier to spell)
-    
+
     var token = "N6jC9hEJzTF9RnctCg_sNYHsnJeGGqXljv7PadDwa9cnNkH1l-dyPYCqUZ3j6JFyEBP9kfiiGvNbjdGloQd-0trLXbfSMkA69e1gvRnJM3q5ps_T1Z7-yZxkWg7dYHYx"
     fetch("http://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=food&limit=50&location=Houston&radius=40000", {
-      // mode:"cors",
-      headers: {
-        'x-requested-with': '123',
-        "Authorization": `Bearer ${token}`,
-        // "Content-Type": "application/x-www-form-urlencoded"
-        // content-type: application/json
-        // server: envoy
-        // x-routing-service: routing-main--useast1-b579557df-26swb; site=public_api_v3
-        // ratelimit-resettime: 2021-07-08T00:00:00+00:00
-        // ratelimit-dailylimit: 5000
-        // ratelimit-remaining: 5000
-        // x-cloudmap: routing_useast1
-        // x-proxied: 10-65-66-189-useast1aprod
-        // x-extlb: 10-65-66-189-useast1aprod
-        // cache-control: max-age=0, no-store, private, no-transform
-        // accept-ranges: bytes
-        // date: Wed, 07 Jul 2021 00:34:22 GMT
-        // via: 1.1 varnish
-        // x-served-by: cache-iah17278-IAH
-        // x-cache: MISS
-        // x-cache-hits: 0
-        // content-length: 49186
-      }
-    })
-    .then(res => res.json())
-    .then(data => console.log(data))
-    // $.ajax({
-    //     url: myurl,
-    //     method: 'GET',
-    //     headers: {
-    //         'accept': 'application/json',
-    //         'x-requested-with': 'xmlhttprequest',
-    //         'Access-Control-Allow-Origin': '*',
-    //         'Authorization': 'Bearer N6jC9hEJzTF9RnctCg_sNYHsnJeGGqXljv7PadDwa9cnNkH1l-dyPYCqUZ3j6JFyEBP9kfiiGvNbjdGloQd-0trLXbfSMkA69e1gvRnJM3q5ps_T1Z7-yZxkWg7dYHYx',
-    //         // 'Content-Type': 'text/html application/json'
-    //     },
+            // mode:"cors",
+            headers: {
+                'x-requested-with': '123',
+                "Authorization": `Bearer ${token}`,
+                // "Content-Type": "application/x-www-form-urlencoded"
+                // content-type: application/json
+                // server: envoy
+                // x-routing-service: routing-main--useast1-b579557df-26swb; site=public_api_v3
+                // ratelimit-resettime: 2021-07-08T00:00:00+00:00
+                // ratelimit-dailylimit: 5000
+                // ratelimit-remaining: 5000
+                // x-cloudmap: routing_useast1
+                // x-proxied: 10-65-66-189-useast1aprod
+                // x-extlb: 10-65-66-189-useast1aprod
+                // cache-control: max-age=0, no-store, private, no-transform
+                // accept-ranges: bytes
+                // date: Wed, 07 Jul 2021 00:34:22 GMT
+                // via: 1.1 varnish
+                // x-served-by: cache-iah17278-IAH
+                // x-cache: MISS
+                // x-cache-hits: 0
+                // content-length: 49186
+            }
+        })
+        .then(res => res.json())
+        .then(data => {
+            var randomNumber = getRandomNumber()
+            console.log(data.businesses)
+            if (searchType === "food") {
+                renderRandomFood(data.businesses[randomNumber])
+            }
+            if (searchType === "bar") {
+                renderRandomBar(data.businesses[randomNumber])
+            }
+        });
+};
 
-    //     // dataType: 'json',
-    //     //when API responds success function is the first thing to run
-    //     success: function (data) {
-    //         var randomNumber = getRandomNumber()
-    //         console.log(data.businesses)
-    //         if(searchType === "food"){
-    //             renderRandomFood(data.businesses[randomNumber])
-    //         }
-    //         if(searchType === "bar"){
-    //             renderRandomBar(data.businesses[randomNumber])
-    //         }
-            
-    //     }
-    // });
-}
 //function to dynamically change city and radius based on user input
 function submitSearchFood() {
     //event.preventDefault();
@@ -69,13 +54,13 @@ function submitSearchFood() {
     var searchTerm = document.getElementById("location").value;
     var miles = document.getElementById("miles").value;
     var meters = 0
-    if(miles >= 25){
-       meters=40000 
+    if (miles >= 25) {
+        meters = 40000
     } else {
-        meters = Math.Floor(miles*1609.344);
+        meters = Math.Floor(miles * 1609.344);
     }
-    var searchRadius = Math.round(meters/1000)*1000
-    
+    var searchRadius = Math.round(meters / 1000) * 1000
+
     getYelpData(searchTerm, searchRadius, "food")
 }
 
@@ -85,13 +70,13 @@ function submitSearchBar() {
     var searchTerm = document.getElementById("location").value;
     var miles = document.getElementById("miles").value;
     var meters = 0
-    if(miles >= 25){
-       meters=40000 
+    if (miles >= 25) {
+        meters = 40000
     } else {
-        meters = Math.Floor(miles*1609.344);
+        meters = Math.Floor(miles * 1609.344);
     }
-    var searchRadius = Math.round(meters/1000)*1000
-    
+    var searchRadius = Math.round(meters / 1000) * 1000
+
     getYelpData(searchTerm, searchRadius, "bar")
 }
 
@@ -137,44 +122,45 @@ function renderRandomBar(randomBar) {
     responseBarContainer.append(randomBarUrl)
 }
 
-function ticketmasterData(city, radiusInput){
-$.ajax({
-    type: "GET",
-    url: "https://app.ticketmaster.com/discovery/v2/events.json?size=50&apikey=6XzGGxlIpYQAZnWYPnzYpZDK59vJeJId&city=Tampa&endDateTime=" + ticketmasterEndDate + "Z",
-    async: true,
-    dataType: "json",
-    success: function (data) {
-        var randomnumber = getRandomNumber()
-        console.log(data)
-        console.log(data._embedded.events[randomnumber].name);
-        renderRandomEvent(data._embedded.events[randomnumber])
-        
-    },
+function ticketmasterData(city, radiusInput) {
+    // $.ajax({
+    //     type: "GET",
+    //     url: "https://app.ticketmaster.com/discovery/v2/events.json?size=50&apikey=6XzGGxlIpYQAZnWYPnzYpZDK59vJeJId&city=Tampa&endDateTime=" + ticketmasterEndDate + "Z",
+    //     async: true,
+    //     dataType: "json",
+    //     success: function (data) {
+    //         var randomnumber = getRandomNumber()
+    //         console.log(data)
+    //         console.log(data._embedded.events[randomnumber].name);
+    //         renderRandomEvent(data._embedded.events[randomnumber])
 
-    method: 'GET',
-    dataType: 'json',
-    //when API responds success function is the first thing to run
-    success: function (data) {
-        var randomNumber = getRandomNumber()
-        console.log(data.businesses)
-        renderRandomBar(data.businesses[randomNumber])
-    }
-});
+    //     },
 
+    // method: 'GET',
+    // dataType: 'json',
+    // //when API responds success function is the first thing to run
+    // success: function (data) {
+    //     var randomNumber = getRandomNumber()
+    //     console.log(data.businesses)
+    //     renderRandomBar(data.businesses[randomNumber])
+    //     }
+    // });
+}
 // function that takes the user input for city and radius and has ticketmaster data meet those parameters
-function searchEvent(){
+function searchEvent() {
     var city = document.querySelector("#location").value;
     var radius = document.querySelector("#miles").value;
     var miles = 0;
     if (radius > 25) {
         miles = 40000
     } else {
-        miles = Math.floor(radius*1609.344);
+        miles = Math.floor(radius * 1609.344);
     }
-    var radiusInput = Math.round(miles/1000)*1000
+    var radiusInput = Math.round(miles / 1000) * 1000
 
     ticketmasterData(city, radiusInput);
 }
+
 function renderRandomEvent(randomEvent) {
     console.log(randomEvent)
     var eventEl = $("<div>").addClass("random-event");
@@ -189,21 +175,20 @@ function renderRandomEvent(randomEvent) {
     ticketmasterUrl.attr("src", "url")
     eventEl.append(ticketmasterUrl)
 }
->>>>>>> 55fb122d7631bd3ef38fae65ff3a695a85888add
+
 //Button function
-var displaybtn = function(){
-  var button = document.createElement('button');
-  button.innerHTML = 'Click for Movies';
-  button.onclick = function(){
-  };
-  
-  document.getElementById('Moviebtn').appendChild(button);
+var displaybtn = function () {
+    var button = document.createElement('button');
+    button.innerHTML = 'Click for Movies';
+    button.onclick = function () {};
+
+    document.getElementById('Moviebtn').appendChild(button);
 };
 
 
 // document.getElementById("Moviebtn").addEventListener("click", displaybtn);
 
-function displaybtn2(){
+function displaybtn2() {
     FindFoodDrinks.style.background = "red";
 }
 
